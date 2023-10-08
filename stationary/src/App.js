3a19676b4686dfcfe456a52404b2e3c680716f60
divyakolape <147237838+divyakolape@users.noreply.github.com>
2023-10-08 09:10:33 +0000

    added name

--- stationary/src/App.js
+++ stationary/src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>deepak bhau</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         hey there we started a project
         </p>
         <a
           className="App-link"
