b8166da0e2b3a2789a082e3d5d5e71e4e0d8d708
Vishnu Gajmal <139453290+vishnugajmal07@users.noreply.github.com>
2023-10-08 09:52:57 +0000

    added a hello heading

--- stationary/src/App.js
+++ stationary/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>deepak bhau</h1>
         <h2>stationary  </h2>
-        <h3>Hello </h3>
+        <h3>Hello I am vishnu </h3>
         <p>
          hey there we started a project
         </p>
